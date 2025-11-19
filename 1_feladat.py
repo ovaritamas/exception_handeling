6620909c81c751bf7c4fcc1c7d4758b404186ee9
ovaritamas <ovari.tamas@diak.szbi-pg.hu>
2025-11-19 08:46:29 +0100

    2.feladat

---
+++ 1_feladat.py
@@ -0,0 +1,12 @@
+osztando = 10
+while True:
+    try:
+        oszto = int(input(f"Mennyivel osszam el a(z) {osztando} számot: "))
+        print(f'A {osztando} és {oszto} hányadosa: {osztando/oszto}')
+        break
+    except ZeroDivisionError as e:
+        print(e)
+        print("Ne osszá nullával")
+    except ValueError as e:
+        print(e)
+        print("Számot...légyszives...köszii")
--- zero_div_value_error.py
+++ zero_div_value_error.py
@@ -2,5 +2,9 @@ osztando = 10
 try:
     oszto = int(input(f"Mennyivel osszam el a(z) {osztando} számot:"))
     print(f'A {osztando} és {oszto} hányadosa: {osztando/oszto}')
-except ZeroDivisionError:
+except ZeroDivisionError as e:
+    print(e)
     print("Ne osszá nullával")
+except ValueError as e:
+    print(e)
+    print("Számot...légyszives...köszii")
