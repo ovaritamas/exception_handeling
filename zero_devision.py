479462f3bf1430084469baacca8c4cb055cdd3a0
ovaritamas <ovari.tamas@diak.szbi-pg.hu>
2025-11-19 08:17:11 +0100

    Update zero_devision.py

--- zero_devision.py
+++ zero_devision.py
@@ -1,6 +1,8 @@
 osztando = 10
 try:
-    oszto = int(input("Mennyivel osszam el a(z) {osztando} számot:"))
+    oszto = int(input(f"Mennyivel osszam el a(z) {osztando} számot:"))
     print(f'A {osztando} és {oszto} hányadosa: {osztando/oszto}')
 except ZeroDivisionError:
     print("Ne osszá nullával")
+except ValueError:
+    print("Számot...légyszives...köszii")
